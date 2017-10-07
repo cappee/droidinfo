268f3f2ee9bf6a18b53b0bd857604df1dd08328a
P4ndaJ <saymon.dev@gmail.com>
2017-10-07 12:43:41 +0200

    The same of previous which is the same of previous...

--- upload-gh-pages.sh
+++ upload-gh-pages.sh
@@ -23,7 +23,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # add, commit and push files
   git add -f .
   git remote rm origin
-  git remote add origin https://PusherWoodstock:$GH_TOKEN@github.com/WoodstockInc/woodstockinc.github.io.git
+  git remote add origin https://PusherWoodstock:$GH_TOKEN@github.com/k4ppaj/DroidInfo.git
   git add -f .
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages!"
   git push -fq origin master > /dev/null
