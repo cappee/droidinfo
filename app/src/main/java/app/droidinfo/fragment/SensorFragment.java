21eea1dc34bcad0447f5384eaabd77faabf82d9c
k4ppaj <gabr.cappellaro@gmail.com>
2018-06-19 17:46:53 +0200

    Added some sensor

--- app/src/main/java/app/droidinfo/fragment/SensorFragment.java
+++ app/src/main/java/app/droidinfo/fragment/SensorFragment.java
@@ -27,13 +27,9 @@ public class SensorFragment extends Fragment implements SensorEventListener{
 
     private Activity activity;
     private Context context;
-    private RecyclerViewAdapter recyclerViewAdapter;
 
     private SensorManager mSensorManager;
-    private Sensor mSensorAccelerometer;
-    private Sensor mSensorMagnetometer;
-    private Sensor mSensorGyroscope;
-    private Sensor mSensorProximity;
+    private Sensor mSensorAccelerometer, mSensorMagnetometer, mSensorGyroscope, mSensorProximity, mSensorAmbientTemperature, mSensorLight, mSensorPressure, mSensorRelativeHumidity, mSensorDeviceTemperature;
 
     private float mAccelerometerX;
     private float mAccelerometerY;
@@ -45,6 +41,11 @@ public class SensorFragment extends Fragment implements SensorEventListener{
     private float mMagnetometerX;
     private float mMagnetometerY;
     private float mMagnetometerZ;
+    private float mAmbientTemperature;
+    private float mLight;
+    private float mPressure;
+    private float mHumidity;
+    private float mDeviceTemperature;
 
     private SharedPreferences sharedPreferences;
     private String USE_DEFAULT_INFORMATION = "USE_DEFAULT_INFORMATION";
@@ -75,6 +76,11 @@ public class SensorFragment extends Fragment implements SensorEventListener{
         mSensorManager.registerListener(this, mSensorMagnetometer, SensorManager.SENSOR_DELAY_NORMAL);
         mSensorManager.registerListener(this, mSensorGyroscope, SensorManager.SENSOR_DELAY_NORMAL);
         mSensorManager.registerListener(this, mSensorProximity, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mSensorAmbientTemperature, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mSensorLight, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mSensorPressure, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mSensorRelativeHumidity, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mSensorDeviceTemperature, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -96,10 +102,20 @@ public class SensorFragment extends Fragment implements SensorEventListener{
             mSensorMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
             mSensorGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
             mSensorProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+            mSensorAmbientTemperature = mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+            mSensorLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+            mSensorPressure = mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+            mSensorRelativeHumidity = mSensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+            mSensorDeviceTemperature = mSensorManager.getDefaultSensor(Sensor.TYPE_TEMPERATURE);
             mSensorManager.registerListener(this, mSensorAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
             mSensorManager.registerListener(this, mSensorMagnetometer, SensorManager.SENSOR_DELAY_NORMAL);
             mSensorManager.registerListener(this, mSensorGyroscope, SensorManager.SENSOR_DELAY_NORMAL);
             mSensorManager.registerListener(this, mSensorProximity, SensorManager.SENSOR_DELAY_NORMAL);
+            mSensorManager.registerListener(this, mSensorAmbientTemperature, SensorManager.SENSOR_DELAY_NORMAL);
+            mSensorManager.registerListener(this, mSensorLight, SensorManager.SENSOR_DELAY_NORMAL);
+            mSensorManager.registerListener(this, mSensorPressure, SensorManager.SENSOR_DELAY_NORMAL);
+            mSensorManager.registerListener(this, mSensorRelativeHumidity, SensorManager.SENSOR_DELAY_NORMAL);
+            mSensorManager.registerListener(this, mSensorDeviceTemperature, SensorManager.SENSOR_DELAY_NORMAL);
         }
 
         recyclerView = layoutView.findViewById(R.id.recyclerViewSensor);
@@ -108,8 +124,14 @@ public class SensorFragment extends Fragment implements SensorEventListener{
                 getString(R.string.Accelerometer),
                 getString(R.string.Magnetometer),
                 getString(R.string.Gyroscope),
-                getString(R.string.Proximity)
+                getString(R.string.Proximity),
+                getString(R.string.AmbientTemperature),
+                getString(R.string.Light),
+                getString(R.string.Pressure),
+                getString(R.string.Humidity)
         };
+
+        stringValues = new String[] {};
 
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(context.getApplicationContext());
         recyclerView.setLayoutManager(mLayoutManager);
@@ -125,7 +147,11 @@ public class SensorFragment extends Fragment implements SensorEventListener{
                     "x: " + String.format(Locale.ENGLISH, "%.2f", mAccelerometerX) + " m/s2 / y: " + String.format(Locale.ENGLISH, "%.2f", mAccelerometerY) + " m/s2 / z: " + String.format(Locale.ENGLISH, "%.2f", mAccelerometerZ) + " m/s2",
                     "x: " + String.format(Locale.ENGLISH, "%.2f", mMagnetometerX) + " μT / y: " + String.format(Locale.ENGLISH, "%.2f", mMagnetometerY) + " μT / z: " + String.format(Locale.ENGLISH, "%.2f", mMagnetometerZ) + " μT",
                     "x: " + String.format(Locale.ENGLISH, "%.2f", mGyroscopeX) + " rad/s / y: " + String.format(Locale.ENGLISH, "%.2f", mGyroscopeY) + " rad/s / z: " + String.format(Locale.ENGLISH, "%.2f", mGyroscopeZ) + " rad/s",
-                    mProximity
+                    mProximity,
+                    String.valueOf(mAmbientTemperature) + " C°",
+                    String.valueOf(mLight) + " lx",
+                    String.valueOf(mPressure) + " hPa",
+                    String.valueOf(mHumidity) + " %",
             };
         } else {
             stringValues = new String[] {
@@ -134,6 +160,7 @@ public class SensorFragment extends Fragment implements SensorEventListener{
         }
 
         RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(RecyclerViewDataHelper.recyclerViewFragment(stringInformation, stringValues), context);
+        //recyclerViewAdapter.notifyDataSetChanged();
         recyclerView.setAdapter(recyclerViewAdapter);
     }
 
@@ -141,24 +168,47 @@ public class SensorFragment extends Fragment implements SensorEventListener{
     public void onSensorChanged(SensorEvent sensorEvent) {
         Sensor sensor = sensorEvent.sensor;
 
-        if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            mAccelerometerX = sensorEvent.values[0];
-            mAccelerometerY = sensorEvent.values[1];
-            mAccelerometerZ = sensorEvent.values[2];
-        } else if (sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-            mMagnetometerX = sensorEvent.values[0];
-            mMagnetometerY = sensorEvent.values[1];
-            mMagnetometerZ = sensorEvent.values[2];
-        } else if (sensor.getType() == Sensor.TYPE_GYROSCOPE) {
-            mGyroscopeX = sensorEvent.values[0];
-            mGyroscopeY = sensorEvent.values[1];
-            mGyroscopeZ = sensorEvent.values[2];
-        } else if (sensor.getType() == Sensor.TYPE_PROXIMITY) {
-            if (sensorEvent.values[0] < mSensorProximity.getMaximumRange()) {
-                mProximity = getString(R.string.Near) + " (" + sensorEvent.values[0] + ")";
-            } else {
-                mProximity = getString(R.string.Far) + " (" + sensorEvent.values[0] + ")";
-            }
+        switch (sensor.getType()) {
+            case Sensor.TYPE_ACCELEROMETER:
+                mAccelerometerX = sensorEvent.values[0];
+                mAccelerometerY = sensorEvent.values[1];
+                mAccelerometerZ = sensorEvent.values[2];
+                break;
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                mMagnetometerX = sensorEvent.values[0];
+                mMagnetometerY = sensorEvent.values[1];
+                mMagnetometerZ = sensorEvent.values[2];
+                break;
+            case Sensor.TYPE_GYROSCOPE:
+                mGyroscopeX = sensorEvent.values[0];
+                mGyroscopeY = sensorEvent.values[1];
+                mGyroscopeZ = sensorEvent.values[2];
+                break;
+            case Sensor.TYPE_LIGHT:
+                mLight = sensorEvent.values[0];
+                break;
+            case Sensor.TYPE_PRESSURE:
+                mPressure = sensorEvent.values[0];
+                break;
+            case Sensor.TYPE_PROXIMITY:
+                if (sensorEvent.values[0] < mSensorProximity.getMaximumRange()) {
+                    mProximity = getString(R.string.Near) + " (" + sensorEvent.values[0] + ")";
+                } else {
+                    mProximity = getString(R.string.Far) + " (" + sensorEvent.values[0] + ")";
+                }
+                break;
+            case Sensor.TYPE_GRAVITY:
+                break;
+            case Sensor.TYPE_LINEAR_ACCELERATION:
+                break;
+            case Sensor.TYPE_ROTATION_VECTOR:
+                break;
+            case Sensor.TYPE_RELATIVE_HUMIDITY:
+                mHumidity = sensorEvent.values[0];
+                break;
+            case Sensor.TYPE_AMBIENT_TEMPERATURE:
+                mAmbientTemperature = sensorEvent.values[0];
+                break;
         }
 
         getSensorData();
